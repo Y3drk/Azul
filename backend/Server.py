150e349e0e971daa4edf73aed8fb90523e2b166f
Nikodem Korohoda <nikodemko@interia.pl>
2024-12-02 18:04:42 +0100

    hotfix on invalid requests

--- backend/Server.py
+++ backend/Server.py
@@ -9,18 +9,22 @@ class Server:
         self.game = None
 
     def start_game(self, players):
+        if self.game is not None:
+            return {"message": "Game has started already!"}, 400
         self.game = Game(players)
         self.game.initial_setup()
         return {"message": "Game started successfully", "current_state": self.game.get_game_state()}, 200
 
     def end_game(self):
+        if self.game is None:
+            return {"message": "Game not started yet!"}, 400
         result = self.game.get_game_state()
         self.game = None
         return {"message": "Game ended successfully", "current_state": result}, 200
 
     def player_move(self, player_id, move):
         if self.game is None:
-            return {"error": "Game not started yet"}, 400
+            return {"error": "Game not started yet!"}, 400
 
         result = self.game.player_move(player_id, move)
         if result is None:
@@ -45,8 +49,6 @@ def start_game():
     players = data["players_names"]
     if len(players.items()) < 2 or len(players.items()) > 4:
         return jsonify({"error": "Number of players must be between 2 and 4."}), 400
-
-    print(players.items())
 
     response = server.start_game(players)
     return jsonify(response)
